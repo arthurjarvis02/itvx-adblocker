3422aa1b3ae8e1b7c22032b0737a95c25640b0b2
Arthur Jarvis <48388803+p3pi27@users.noreply.github.com>
2024-05-15 15:46:40 +0100

    Fix extension not working when video element loads after content script runs

--- content.js
+++ content.js
@@ -1,50 +1,53 @@
-function removeAds() {
+const logMsg = (msg) => `[ITVX Adblocker] ${msg}`;
 
-    console.log("ITVX Adblocker Active")
+function getIdsFromVideoElement() {
 
-    elem = document.getElementsByClassName("genie-container")[0]
+    const rawId = document.getElementsByClassName("genie-container")[0]?.getAttribute("data-video-id")?.split("/").slice(-1)[0];
 
-    if (elem) {
-
-        attrib = elem.getAttribute("data-video-id")
-
-        if (!attrib) {
-
-            console.log("Video ID not found");
-            return;
-        }
-
-        rawId = attrib.split("/").slice(-1)[0];
-        idBase = rawId.split(".")[0].replaceAll("_", "/");
-        idIndex = rawId.split(".")[1];
-
-        if (!idBase || !idIndex) {
-
-            console.log("Video ID in unknown format");
-            return;
-        }
-
-        console.log("Episode IDs:", rawId, idBase, idIndex);
-
-        timestamp = Date.now() - 60000
-        breaksWatched = {
-            indexes: [...Array(20).keys()],
-            timestamp: timestamp
-        }
-
-        storage = JSON.parse(localStorage.getItem("genie-productions")) || {};
-        storage[idBase] = storage[idBase] || {};
-        storage[idBase][idIndex] = storage[idBase][idIndex] || {};
-        storage[idBase][idIndex].breaksWatched = breaksWatched;
-
-        localStorage.setItem("genie-productions", JSON.stringify(storage));
-
-        console.log("Removed ad breaks 1-20")
-
-    } else {
-
-        console.log("Video element not found");
-    }
+    return [rawId?.split(".")[0].replaceAll("_", "/"), rawId?.split(".")[1]];
 }
 
-removeAds();
+function addFakeBreaksWatched(idBase, idIndex, count=20) {
+
+    const storage = JSON.parse(localStorage.getItem("genie-productions")) || {};
+
+    localStorage.setItem("genie-productions", JSON.stringify(
+        {
+            ...storage,
+            [idBase]: {
+                ...storage[idBase],
+                [idIndex]: {
+                    breaksWatched: {
+                        indexes: [...Array(count).keys()],
+                        timestamp: Date.now()
+                    }
+                }
+            }
+        }
+    ));
+}
+
+const DELAY_MS = 1000;
+const BREAKS_WATCHED_COUNT = 20;
+
+console.log(logMsg(`Waiting ${DELAY_MS}ms`));
+
+setTimeout(() => {
+
+    console.log(logMsg("Running"));
+
+    const [idBase, idIndex] = getIdsFromVideoElement();
+
+    if (!idBase || !idIndex) {
+
+        console.error(logMsg("Video ID not found"));
+        return;
+    }
+
+    console.log(logMsg(`Episode IDs: ${idBase}, ${idIndex}`));
+
+    addFakeBreaksWatched(idBase, idIndex, BREAKS_WATCHED_COUNT);
+
+    console.log(logMsg(`Removed ad breaks 1-${BREAKS_WATCHED_COUNT}`));
+
+}, DELAY_MS);
